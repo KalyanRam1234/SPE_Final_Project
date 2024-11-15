f64ded74fa46d21e2c7ff7253320c78cb5b545d2
LieutPaul <Vikas.Kalyanapuram@iiitb.ac.in>
2024-11-15 22:00:02 +0530

    Add Docker Compose for student and booklending app

--- Library-Backend/BookLendingApp/src/main/java/com/Dockerates/BookLending/Constants.java
+++ Library-Backend/BookLendingApp/src/main/java/com/Dockerates/BookLending/Constants.java
@@ -1,8 +1,6 @@
 package com.Dockerates.BookLending;
 
 public class Constants {
-    public static String StudentUrl="http://3.110.217.62:80/";
-    public static String BookUrl="http://13.127.251.149:80/";
-
-
+    public static String StudentUrl = "http://studentapp:8081/";
+    public static String BookUrl = "http://bookapp:8080/";
 }
