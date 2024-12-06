ee1b1635b09e4ab19f9523ec04ab808d33965bb8
LieutPaul <Vikas.Kalyanapuram@iiitb.ac.in>
2024-12-06 16:06:56 +0530

    Configure k8s

--- Library-Backend/BookLendingApp/src/main/java/com/Dockerates/BookLending/Constants.java
+++ Library-Backend/BookLendingApp/src/main/java/com/Dockerates/BookLending/Constants.java
@@ -1,8 +1,8 @@
 package com.Dockerates.BookLending;
 
 public class Constants {
-    public static String StudentUrl = "http://studentapp:8081/";
-    public static String BookUrl = "http://bookapp:8082/";
-    // public static String StudentUrl = "http://localhost:8081/";
-    // public static String BookUrl = "http://localhost:8082/";
+    // public static String StudentUrl = "http://studentapp:8081/";
+    // public static String BookUrl = "http://bookapp:8082/";
+    public static String StudentUrl = "http://localhost:8081/";
+    public static String BookUrl = "http://localhost:8082/";
 }
