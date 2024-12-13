537ca9ea0b97116023cc5a16961af3047d3fcee7
KalyanRam1234 <kalyanrammunagala@gmail.com>
2024-12-13 18:28:25 +0530

    Updated resources

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -7,8 +7,9 @@ kubectl apply -f studentapp-infra
 kubectl apply -f booklendingapp-infra
 kubectl apply -f frontend-infra
 # kubectl apply -f ingress-infra
-kubectl apply -f ingress-infra/prometheus-ingress.yaml
-kubectl apply -f ingress-infra/frontend-ingress.yaml
 
 kubectl apply -f prometheus
 kubectl apply -f kube-state-metrics-configs
+
+kubectl apply -f ingress-infra/prometheus-ingress.yaml
+kubectl apply -f ingress-infra/frontend-ingress.yaml
