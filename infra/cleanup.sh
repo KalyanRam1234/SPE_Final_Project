72d7e291cb9ae67757f0e1d30dbcdbd467189d35
KalyanRam1234 <kalyanrammunagala@gmail.com>
2024-12-14 11:04:37 +0530

    configuration updated

--- infra/cleanup.sh
+++ infra/cleanup.sh
@@ -17,7 +17,7 @@ kubectl delete svc bookapp-svc || true
 kubectl delete svc studentapp-svc || true
 kubectl delete svc booklendingapp-svc || true
 kubectl delete svc library-frontend-svc || true
-kubectl delete svc mysql || true
+# kubectl delete svc mysql || true
 kubectl delete svc prometheus-service -n monitoring || true
 kubectl delete svc kube-state-metrics -n kube-system || true
 
